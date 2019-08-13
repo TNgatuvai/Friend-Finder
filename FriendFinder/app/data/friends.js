d827b475dd32aea531f3b1335dfd83c19b1dcf7f
Tali Ngatuvai <tali_ngatuvai@Talis-MacBook-Pro.local>
2019-08-12 23:34:03 -0600

    Completed friends.js

---
+++ FriendFinder/app/data/friends.js
@@ -0,0 +1,75 @@
+var friendsArray = [
+    {
+        name: 'Scooter',
+        photo: 'http://cdn3-www.dogtime.com/assets/uploads/gallery/30-impossibly-cute-puppies/impossibly-cute-puppy-15.jpg',
+        scores: [
+            '1',
+            '5',
+            '5',
+            '1',
+            '5'
+        ]
+    },
+    {
+        name: 'Fluffy',
+        photo: 'https://i.ytimg.com/vi/mRf3-JkwqfU/hqdefault.jpg',
+        scores: [
+            '2',
+            '4',
+            '1',
+            '1',
+            '1'
+        ]
+    },
+
+    {
+        name: 'Mrs. Piggy',
+        photo: 'http://img.mindbodygreen.com/image/upload/c_limit,w_738,f_auto/ftr/Miss-Piggy-large-tonya-312.jpg',
+        scores: [
+            '1',
+            '1',
+            '5',
+            '1',
+            '3'
+        ]
+    },
+    {
+        name: 'Beyonce Knowles',
+        photo: 'http://www.billboard.com/files/styles/article_main_image/public/media/beyonce-feb-2014-billboard-650.jpg',
+        scores: [
+            '5',
+            '4',
+            '4',
+            '2',
+            '4'
+        ]
+    },
+
+    {
+        name: 'Kermit the Frog',
+        photo: 'http://i.dailymail.co.uk/i/pix/2015/09/02/08/2BE5406100000578-3218820-image-m-25_1441179775589.jpg',
+        scores: [
+            '5',
+            '5',
+            '5',
+            '5',
+            '5'
+        ]
+    },
+    {
+        name: "Chandler Bing",
+        photo: "http://33.media.tumblr.com/cf9686363fde6ca698a9b5b603eef01a/tumblr_ms0bxt7ThZ1r9zaago1_500.gif",
+        scores: [
+            "4",
+            "4",
+            "2",
+            "3",
+            "2"
+        ]
+    }
+
+];
+
+
+
+module.exports = friendsArray;
