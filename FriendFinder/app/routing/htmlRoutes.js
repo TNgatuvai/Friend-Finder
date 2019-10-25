09584369979e2f3ca30fcc2c422bd72a51102ed2
Tali Ngatuvai <tali_ngatuvai@Talis-MacBook-Pro.local>
2019-10-25 12:43:41 -0600

    formating updates

--- FriendFinder/app/routing/htmlRoutes.js
+++ FriendFinder/app/routing/htmlRoutes.js
@@ -2,7 +2,7 @@ var path = require('path');
 
 //ROUTING
 
-module.exports = function(app){
+module.exports = function (app) {
   //default GET route that leads to home.html - displays home page
   app.get('/survey', function (req, res) {
     res.sendFile(path.join(__dirname + '/../public/survey.html'));
